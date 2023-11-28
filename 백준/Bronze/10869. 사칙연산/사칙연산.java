0b4bc83b8299b6cea85aae3a1f0d8bdd1c92cf23
fever-max <131164588+fever-max@users.noreply.github.com>
2023-11-28 09:28:52 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt(); // 7
+		int b = scanner.nextInt(); // 3
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+
+}
