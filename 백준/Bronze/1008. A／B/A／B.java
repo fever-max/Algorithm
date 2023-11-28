8f3c298942087f314fc75cbbed6676a57f19e66b
fever-max <131164588+fever-max@users.noreply.github.com>
2023-11-28 09:24:34 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17912 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double a = scanner.nextInt();
+		double b = scanner.nextInt();
+
+		System.out.printf("%.9f", a / b);
+	}
+
+}
