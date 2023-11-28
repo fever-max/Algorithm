2960e236ec975d72be2714c2c360ed4a38aff9bc
fever-max <131164588+fever-max@users.noreply.github.com>
2023-11-28 18:54:38 +0900

    [level unrated] Title: 더 크게 합치기, Time: 7.68 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int solution(int a, int b) {
+		int answer = 0;
+		String num1 = a + "" + b;
+		String num2 = b + "" + a;
+
+		if (Integer.parseInt(num1) > Integer.parseInt(num2)) {
+			answer = Integer.parseInt(num1);
+		} else if (Integer.parseInt(num1) < Integer.parseInt(num2)) {
+			answer = Integer.parseInt(num2);
+		} else {
+			answer = Integer.parseInt(num1);
+		}
+		return answer;
+	}
+}
