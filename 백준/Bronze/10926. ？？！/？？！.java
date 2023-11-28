f29408241dd47b642737f8600576326e274f6ea2
fever-max <131164588+fever-max@users.noreply.github.com>
2023-11-28 09:34:59 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.next();
+
+		System.out.printf("%s??!", name);
+
+	}
+
+}
