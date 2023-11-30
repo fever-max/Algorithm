1199763a1938e64c632743acec6304b19795f29b
fever-max <131164588+fever-max@users.noreply.github.com>
2023-11-30 18:07:41 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17892 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -2,17 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int num[] = new int[2];
-
-		for (int i = 0; i < n; i++) {
-			num[0] = scanner.nextInt();
-			num[1] = scanner.nextInt();
-			System.out.println(num[0] + num[1]);
-		}
-
-	}
+        for (int i = 0; i < n; i++) {
+            System.out.println(scanner.nextInt() + scanner.nextInt());
+        }
+    }
 }
