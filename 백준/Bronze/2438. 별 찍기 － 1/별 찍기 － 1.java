6d8b6ddbd209c81b87ac88136f7f644055a408b8
fever-max <131164588+fever-max@users.noreply.github.com>
2023-12-01 14:46:25 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 316 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+
+		for (int i = 0; i < num; i++) {
+
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
