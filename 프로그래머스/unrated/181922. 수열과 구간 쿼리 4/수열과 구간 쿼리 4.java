f5eca6524140014dd5506f451a2cc0d220ed75c2
fever-max <131164588+fever-max@users.noreply.github.com>
2023-12-08 15:47:04 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 1.22 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,23 +1,20 @@
+
 class Solution {
-	public int[] solution(int[] arr, int[][] queries) {
-		int[] answer = new int[arr.length];
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        for(int i=0;i<queries.length;i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
 
-		for (int[] query : queries) {
-			int start = query[0];
-			int end = query[1];
-			int k = query[2];
+            for(int j=s;j<=e;j++){
+                if(j%k==0){
+                    arr[j]+=1;
+                }
+            }
+        }
 
-			for (int i = start; i <= end; i++) {
-				if (i % k == 0) {
-					answer[i]++;
-				}
-			}
-		}
-
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] += answer[i];
-		}
-
-		return arr;
-	}
+        answer = arr;
+        return answer;
+    }
 }
