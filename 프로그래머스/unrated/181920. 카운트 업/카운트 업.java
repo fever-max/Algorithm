4d6d8f100ce3139a4e1876fb10de143c6c7ace05
fever-max <131164588+fever-max@users.noreply.github.com>
2023-12-11 19:12:05 +0900

    [level unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -2,10 +2,10 @@ class Solution {
 	public int[] solution(int start_num, int end_num) {
 		int[] nums = new int[(end_num - start_num) + 1];
 
-		for (int i = start_num; i <= end_num; i++) {
-			int index = i - start_num;
-			nums[index] = i;
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = start_num + i;
 		}
+
 		return nums;
 	}
 }
