6abbdf46e6b663ea83c36fbadaf93383c19638a6
fever-max <131164588+fever-max@users.noreply.github.com>
2023-12-12 17:52:52 +0900

    [level unrated] Title: 배열 만들기 4, Time: 4.07 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -5,18 +5,16 @@ class Solution {
 		ArrayList<Integer> stk = new ArrayList<>();
 		int i = 0;
 
-		while (i < arr.length) {
+		while (i < arr.length) { // 빈 배열 이라면
 			if (stk.size() == 0) {
-                //str이 빈 배열 이라면
 				stk.add(arr[i]);
 				i++;
 			} else if (stk.get(stk.size() - 1) < arr[i]) {
-                //str 마지막 인덱스가 arr[i]보다 작으면
-				int index = (i + 1 <= stk.size()) ? i + 1 : stk.size();
-				stk.add(index, arr[i]);
+				// str 마지막 인덱스가 arr[i]보다 작으면
+				stk.add(arr[i]);
 				i++;
 			} else if (stk.get(stk.size() - 1) >= arr[i]) {
-                //str 마지막 인덱스가 arr[i]보다 크거나 같으면
+				// str 마지막 인덱스가 arr[i]보다 크거나 같으면
 				stk.remove(stk.size() - 1);
 			}
 		}
