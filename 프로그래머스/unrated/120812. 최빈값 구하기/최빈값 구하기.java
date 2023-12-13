f9d9db2b8dadbb2e251f8cb1f5af09e214972aa6
fever-max <131164588+fever-max@users.noreply.github.com>
2023-12-13 17:03:03 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.13 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -3,18 +3,24 @@ class Solution {
     public int solution(int[] array) {
         int maxCount = 0;
         int answer = 0;
+
         Map<Integer, Integer> map = new HashMap<>();
-        for(int number : array){
+
+        for(int number : array) {
             int count = map.getOrDefault(number, 0) + 1;
-            if(count > maxCount){
+
+            if(count > maxCount) {
                 maxCount = count;
                 answer = number;
             }
-            else  if(count == maxCount){
+
+            else if(count == maxCount) {
                 answer = -1;
             }
+
             map.put(number, count);
         }
+
         return answer;
     }
 }
