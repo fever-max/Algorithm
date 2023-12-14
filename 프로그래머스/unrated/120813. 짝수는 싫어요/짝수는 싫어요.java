79cd7d043e71b0c597e0dfcca3257dd66e76ee33
fever-max <131164588+fever-max@users.noreply.github.com>
2023-12-14 18:07:31 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+	public ArrayList<Integer> solution(int n) {
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 != 0) {
+				answer.add(i);
+			}
+		}
+		return answer;
+	}
+}
