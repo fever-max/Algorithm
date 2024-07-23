594644219b59d937f527b500ce1ae5bd962af4b6
Hyemin Lee <fever_max@naver.com>
2024-07-23 11:33:35 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,6 +1,9 @@
-import java.util.Arrays;
 class Solution {
     public double solution(int[] arr) {
-        return Arrays.stream(arr).average().orElse(0.0);
+        int sum = 0;
+        for (int a : arr) {
+            sum += a;
+        }
+        return  (double) sum / arr.length;
     }
 }
