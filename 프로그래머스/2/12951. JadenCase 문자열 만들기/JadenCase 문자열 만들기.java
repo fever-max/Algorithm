a030d40ddd8295b2d3251a718a9103abac8c7100
Hyemin Lee <fever_max@naver.com>
2024-07-25 22:04:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,24 +1,24 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         boolean b = true;
         
         for (char c : s.toCharArray()) {
             //전체 문자 순회
             if (c == ' ') {
                 //공백이면 그대로 입력
-                answer += c;
+                answer.append(c);
                 b = true;
             } else if (b) {
                 //공백 다음에 오는 문자는 대문자로 처리
-                answer += Character.toUpperCase(c);
+                answer.append(Character.toUpperCase(c));
                 b = false;
             } else {
                 //다른 글자들은 소문자로
-                answer += Character.toLowerCase(c);
+                answer.append(Character.toLowerCase(c));
             }
         }
 
-        return answer;
+        return answer.toString();
     }
 }
